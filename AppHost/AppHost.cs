37d166bf3d3ab3695ee5e8f89d48ef43fbeb343b
Brian Sheridan <brian.sheridan@gmail.com>
2025-09-29 14:42:17 -0700

    Add Job-DateInfo project with scheduled Azure Container App job configuration

--- AppHost/AppHost.cs
+++ AppHost/AppHost.cs
@@ -4,4 +4,9 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 builder.AddAzureContainerAppEnvironment("aca-env").WithAzdResourceNaming();
 
+#pragma warning disable ASPIREAZURE002 // Type is for evaluation purposes only and is subject to change or removal in future updates
+builder.AddProject<Projects.Job_DateInfo>("dateinfo-job")
+    .PublishAsScheduledAzureContainerAppJob("0 */1 * * *");
+#pragma warning restore ASPIREAZURE002
+
 builder.Build().Run();
---
+++ Job-DateInfo/Program.cs
@@ -0,0 +1,7 @@
+using Job_DateInfo;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
